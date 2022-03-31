f77765e2668060b8e9170c2f86cb09513529b176
aushamim <au.shamim0@gmail.com>
2022-04-01 01:19:22 +0600

    Added Icon & Logo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div className="bg-white p-3 h-screen flex">
-      <div>
+    <div className="bg-white p-3 pl-0 h-screen flex">
+      <div className="">
         <Navigation></Navigation>
       </div>
       <div className="bg-[#f5f6fb] p-10 rounded-lg w-full">
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-  return <div className="p-5">aksjlkdja</div>;
+  return (
+    <div className="p-5 pt-0">
+      <Link to="/">
+        <lottie-player
+          src="https://assets9.lottiefiles.com/packages/lf20_n2yhd0lo.json"
+          background="transparent"
+          speed="1"
+          style={{ width: "60px", height: "60px" }}
+          loop
+          autoplay
+        ></lottie-player>
+      </Link>
+    </div>
+  );
 };
 
 export default Navigation;
