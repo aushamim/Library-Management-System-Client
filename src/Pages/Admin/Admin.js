e02b8dac88797310de1001023615c083cdda115b
ainayeem <ain.nayeem1999@gmail.com>
2022-04-11 23:19:08 +0600

    table edited

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -24,7 +24,7 @@ const Admin = () => {
             <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
               <div className="overflow-hidden rounded-md">
                 <table className="min-w-full text-center">
-                  <thead className="border-b bg-gray-200 font-semibold">
+                  <thead className="border-b bg-gray-300 font-semibold">
                     <tr>
                       <th
                         scope="col"
@@ -60,19 +60,19 @@ const Admin = () => {
                   </thead>
                   <tbody>
                     <tr className="bg-white border-b">
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-mono">
                         asdfghjklo
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-gray-900 px-6 py-4 whitespace-nowrap">
                         Ashikul Islam
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-gray-900 px-6 py-4 whitespace-nowrap">
                         ai.nayeem@gmail.com
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-gray-900 px-6 py-4 whitespace-nowrap">
                         user
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap flex items-center justify-center">
+                      <td className="text-sm text-gray-900 px-6 py-4 whitespace-nowrap flex items-center justify-center">
                         <button className="bg-emerald-100 hover:bg-emerald-300 transition ease-in-out duration-500 shadow-sm p-2 rounded-md font-semibold">
                           Promote
                         </button>
@@ -110,7 +110,7 @@ const Admin = () => {
             <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
               <div className="overflow-hidden rounded-md">
                 <table className="min-w-full text-center">
-                  <thead className="border-b bg-gray-200 font-semibold">
+                  <thead className="border-b bg-gray-300 font-semibold">
                     <tr>
                       <th
                         scope="col"
@@ -140,16 +140,16 @@ const Admin = () => {
                   </thead>
                   <tbody>
                     <tr className="bg-white border-b">
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 font-mono">
                         asdfghjklo
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-gray-900 px-6 py-4 whitespace-nowrap">
                         Ashikul Islam
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      <td className="text-sm text-gray-900 px-6 py-4 whitespace-nowrap">
                         ai.nayeem@gmail.com
                       </td>
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap flex items-center justify-center">
+                      <td className="text-sm text-gray-900 px-6 py-4 whitespace-nowrap flex items-center justify-center">
                         <button className="bg-red-100 hover:bg-red-300 transition ease-in-out duration-500 shadow-sm p-2 rounded-md font-semibold">
                           Delete
                         </button>
